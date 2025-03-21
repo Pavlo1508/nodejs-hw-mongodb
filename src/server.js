f0a25909743aa30612b1c537aa7b8e7476a79568
PKdev <pasha150898@gmail.com>
2025-03-21 02:38:55 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -55,6 +55,12 @@ export const setupServer = () => {
     });
 	});
 
+	app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 	});
