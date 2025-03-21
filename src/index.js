43183930843d2d9d827913472ac935a6635d6ed4
PKdev <pasha150898@gmail.com>
2025-03-21 02:15:21 +0100

    Created MongoDB database and connected it

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-// src/index.js
-
+import { initMongoDB } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
-setupServer();
+const bootstrap = async () => {
+  await initMongoDB();
+  setupServer();
+};
+
+bootstrap();
--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
-const PORT = Number(getEnvVar('PORT', '3000'));
+const PORT = Number(getEnvVar('PORT', '8080'));
 
 export const setupServer = () => {
   const app = express();
@@ -20,28 +20,37 @@ export const setupServer = () => {
         target: 'pino-pretty',
       },
     }),
-  );
+	);
+	
+	app.get('/contacts', async (req, res) => {
+		const contacts = await getAllContacts();
 
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello world!',
+		res.status(200).json({
+      message: 'Successfully found contacts!',
+      data: contacts,
     });
-  });
+	});
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+	app.get('/contacts/:contactId', async (req, res) => {
+		const { contactId } = req.params;
+		const contact = await getContactById(contactId);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
+		if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+		res.status(200).json({
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
-  });
+	});
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-  });
+	});
 };
+
+
