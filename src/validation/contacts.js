84d84563b19b3b5ab7b12bdc48562e6e482aa0b6
PKdev <pasha150898@gmail.com>
2025-03-31 16:06:23 +0200

    HW4. Validation

---
+++ src/validation/contacts.js
@@ -0,0 +1,25 @@
+// src/validation/contacts.js
+
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  _id: Joi.string().required(),
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.number().integer().required(),
+  email: Joi.string().min(3).max(20).allow(null, ''),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string()
+    .min(3)
+    .max(20)
+    .valid('work', 'home', 'personal')
+    .required(),
+});
+
+export const updateContactSchema = Joi.object({
+  _id: Joi.string(),
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.number().integer(),
+  email: Joi.string().min(3).max(20).allow(null, ''),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal'),
+});
