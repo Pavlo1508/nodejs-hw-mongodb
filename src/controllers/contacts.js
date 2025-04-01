b2107670bf0fb81f92eaa9568db8017eae8c0cb6
PKdev <pasha150898@gmail.com>
2025-04-01 05:19:17 +0200

    Added throw mechanism and update errorHandler and contact model

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -15,81 +15,96 @@ import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export const getContactsController = async (req, res, next) => {
-	const { page, perPage } = parsePaginationParams(req.query);
-	const { sortBy, sortOrder } = parseSortParams(req.query);
-	const filter = parseFilterParams(req.query);
+  try {
+    const { page, perPage } = parsePaginationParams(req.query);
+    const { sortBy, sortOrder } = parseSortParams(req.query);
+    const filter = parseFilterParams(req.query);
 
-  const contacts = await getAllContacts({
-    page,
-    perPage,
-    sortBy,
-    sortOrder,
-    filter,
-  });
+    const contacts = await getAllContacts({
+      page,
+      perPage,
+      sortBy,
+      sortOrder,
+      filter,
+    });
 
-  res.status(200).json({
-    status: 200,
-    message: 'Successfully found contacts!',
-    data: contacts,
-  });
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getContactByIdController = async (req, res, next) => {
-  const { contactId } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(contactId)) {
-    return res.status(400).json({
-      status: 400,
-      message: 'Invalid contact ID format',
+  try {
+    const { contactId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(contactId)) {
+      throw createHttpError(400, 'Invalid contact ID format');
+    }
+
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      throw createHttpError(404, 'Contact not found');
+    }
+
+    res.json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
+  } catch (error) {
+    next(error);
   }
-
-  const contact = await getContactById(contactId);
-  if (!contact) {
-    throw createHttpError(404, 'Contact not found');
-  }
-
-  res.json({
-    status: 200,
-    message: `Successfully found contact with id ${contactId}!`,
-    data: contact,
-  });
 };
 
-export const createContactController = async (req, res) => {
-  const student = await createContact(req.body);
+export const createContactController = async (req, res, next) => {
+  try {
+    const student = await createContact(req.body);
 
-  res.status(201).json({
-    status: 201,
-    message: `Successfully created a contact!`,
-    data: student,
-  });
+    res.status(201).json({
+      status: 201,
+      message: `Successfully created a contact!`,
+      data: student,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const patchContactController = async (req, res, next) => {
-  const { contactId } = req.params;
-  const result = await updateContact(contactId, req.body);
+  try {
+    const { contactId } = req.params;
+    const result = await updateContact(contactId, req.body);
 
-  if (!result) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    if (!result) {
+      throw createHttpError(404, 'Contact not found');
+    }
+
+    res.json({
+      status: 200,
+      message: `Successfully patched a contact!`,
+      data: result.contact,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  res.json({
-    status: 200,
-    message: `Successfully patched a contact!`,
-    data: result.contact,
-  });
 };
 
 export const deleteContactController = async (req, res, next) => {
-  const { contactId } = req.params;
+  try {
+    const { contactId } = req.params;
 
-  const contact = await deleteContact(contactId);
+    const contact = await deleteContact(contactId);
 
-  if (!contact) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    if (!contact) {
+      throw createHttpError(404, 'Contact not found');
+    }
+
+    res.status(204).send();
+  } catch (error) {
+    next(error);
   }
-
-  res.status(204).send();
 };
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,18 +1,18 @@
 // src/middlewares/errorHandler.js
-import { HttpError } from 'http-errors';
+import createHttpError from 'http-errors';
 
-export const errorHandler = (err, req, res, next) => {	
-	if (err instanceof HttpError) {
+export const errorHandler = (err, req, res, next) => {
+  if (createHttpError.isHttpError(err)) {
     res.status(err.status).json({
       status: err.status,
       message: err.name,
       data: err,
     });
     return;
-	}
-	
-	res.status(500).json({
-		status: 500,
+  }
+
+  res.status(500).json({
+    status: 500,
     message: 'Something went wrong',
     error: err.message,
   });
