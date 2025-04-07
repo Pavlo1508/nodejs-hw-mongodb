e6c956e56e0778d62a25389ae92e425ead727daa
PKdev <pasha150898@gmail.com>
2025-04-07 20:43:26 +0200

    HW05 - refresh and logout

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -7,6 +7,8 @@ import { validateBody } from '../middlewares/validateBody.js';
 import { registerUserController } from '../controllers/auth.js';
 import { loginUserSchema } from '../validation/auth.js';
 import { loginUserController } from '../controllers/auth.js';
+import { logoutUserController } from '../controllers/auth.js';
+import { refreshUserSessionController } from '../controllers/auth.js';
 
 const router = Router();
 
@@ -21,5 +23,9 @@ router.post(
   validateBody(loginUserSchema),
   ctrlWrapper(loginUserController),
 );
+
+router.post('/logout', ctrlWrapper(logoutUserController));
+
+router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
 export default router;
