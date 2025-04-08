7621b26599258f5639bf8c9720fe31901e4c1cb7
PKdev <pasha150898@gmail.com>
2025-04-08 11:19:21 +0200

    HW06 - passreset

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -75,3 +75,14 @@ export const refreshUserSessionController = async (req, res) => {
     },
   });
 };
+
+import { requestResetToken } from '../services/auth.js';
+
+export const requestResetEmailController = async (req, res) => {
+  await requestResetToken(req.body.email);
+  res.json({
+    message: 'Reset password email was successfully sent!',
+    status: 200,
+    data: {},
+  });
+};
