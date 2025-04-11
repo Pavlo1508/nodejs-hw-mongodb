73e88f28b47ad0db4db1392d48debfaf3b70c3e1
PKdev <pasha150898@gmail.com>
2025-04-11 11:45:55 +0200

    Changed response error code from 404 to 401

--- src/services/auth.js
+++ src/services/auth.js
@@ -24,7 +24,7 @@ export const registerUser = async (payload) => {
 export const loginUser = async (payload) => {
   const user = await UsersCollection.findOne({ email: payload.email });
   if (!user) {
-    throw createHttpError(404, 'User not found');
+    throw createHttpError(401, 'Invalid email or password');
   }
   const isEqual = await bcrypt.compare(payload.password, user.password);
 
