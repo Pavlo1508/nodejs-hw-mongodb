40dc45d4b94748df20047470be2968fdf6e4fb5b
PKdev <pasha150898@gmail.com>
2025-04-13 11:55:55 +0200

    .

--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -1,16 +1,15 @@
 // src/utils/getEnvVar.js
 
 import dotenv from 'dotenv';
-import path from 'path';
 
-dotenv.config({
-  path: path.resolve(process.cwd(), '.env'),
-});
+dotenv.config();
 
 export function getEnvVar(name, defaultValue) {
   const value = process.env[name];
+
   if (value) return value;
+
   if (defaultValue) return defaultValue;
+
   throw new Error(`Missing: process.env['${name}'].`);
 }
-
