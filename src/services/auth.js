9e727f630c06e10eab8e753d5203b22cdb7c0dac
PKdev <pasha150898@gmail.com>
2025-04-14 10:53:13 +0200

    changed endpoints for reset pass

--- src/services/auth.js
+++ src/services/auth.js
@@ -127,7 +127,7 @@ export const requestResetToken = async (email) => {
   const template = handlebars.compile(templateSource);
   const html = template({
     name: user.name,
-    link: `${getEnvVar('APP_DOMAIN')}/reset-password?token=${resetToken}`,
+    link: `${getEnvVar('APP_DOMAIN')}/reset-pwd?token=${resetToken}`,
   });
 
   try {
