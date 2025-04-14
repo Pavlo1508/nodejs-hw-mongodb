81c8ff205bbf655ff0a33328d34ac917e545d83e
PKdev <pasha150898@gmail.com>
2025-04-14 11:23:44 +0200

    changed reset routes

--- src/services/auth.js
+++ src/services/auth.js
@@ -126,7 +126,7 @@ export const requestResetToken = async (email) => {
   const template = handlebars.compile(templateSource);
   const html = template({
     name: user.name,
-    link: `${getEnvVar('APP_DOMAIN')}/reset-password?token=${resetToken}`,
+    link: `${getEnvVar('APP_DOMAIN')}/reset-pwd?token=${resetToken}`,
   });
 
   try {
